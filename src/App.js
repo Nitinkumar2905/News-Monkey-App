4725317c4b7e975f8109582e740fc1da51c5b981
Nitin kumar <nimble2905@gmail.com>
2023-04-16 22:54:27 +0530

    some changes in app files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
-
 import React, { Component } from 'react'
+import Navbar from './Components/Navbar';
+import News from './Components/News';
 
 export default class App extends Component {
-  c= 'nitin'
   render() {
     return (
-      <div>
-        hey class based component {this.c}
-      </div>
+      <>
+        <Navbar/>
+        <News/>
+      </>
     )
   }
 }
