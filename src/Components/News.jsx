cd24a57ff8465a0a36fe1695be398a7ffffc2b23
Nitin kumar <nimble2905@gmail.com>
2023-04-16 22:55:46 +0530

    news component

---
+++ src/Components/News.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+// import PropTypes from 'prop-types'
+
+export class News extends Component {
+  static propTypes = {
+
+  }
+
+  render() {
+    return (
+      <div>
+        hey
+        <NewsItem/>
+      </div>
+    )
+  }
+}
+
+export default News
