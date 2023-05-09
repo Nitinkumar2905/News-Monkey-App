fd47c3ebfc34c100f6176f25d5022287c1a290d5
Nitin kumar <nimble2905@gmail.com>
2023-05-10 00:39:13 +0530

    adding news content

--- src/Components/NewsItem.jsx
+++ src/Components/NewsItem.jsx
@@ -3,15 +3,15 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let {title, description, imageUrl} = this.props
+    let {title, description, imageUrl, newsUrl} = this.props
     return (
       <>
-        <div className="card" style={{width: "18rem"}}>
-          <img src={imageUrl} className="card-img-top" alt="..."/>
+        <div className="card my-2" style={{width: "18rem"}}>
+          <img src={!imageUrl?"https://data1.ibtimes.co.in/en/full/784804/sony-shut-pixelopus-developer-video-game-concrete-genie.jpg":imageUrl} className="card-img-top" alt="..."/>
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <a href="/" className="btn btn-sm btn-primary">Read More</a>
+            <h5 className="card-title">{title}....</h5>
+            <p className="card-text">{description}....</p>
+            <a href={newsUrl} target='_blank' rel="noreferrer" className="btn btn-sm btn-dark">Read More</a>
           </div>
         </div>
       </>
