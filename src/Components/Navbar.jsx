cf2b976586deddeb8febb27236c688919edcd376
Nitin kumar <nimble2905@gmail.com>
2023-05-14 22:57:51 +0530

    added logo and favicon

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from './logo.jpeg';
 // import PropTypes from 'prop-types'
 
 const Navbar=()=>{
@@ -7,7 +8,10 @@ const Navbar=()=>{
       <>
         <nav className="navbar navbar-expand-lg  navbar-dark bg-dark" style={{position:'sticky', top:'0px', zIndex:'10'}}>
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/">NewsMonkey</Link>
+                <Link className="navbar-brand" to="/">
+                  <img src={logo} style={{height:'40px', borderRadius:'40px', marginRight:'10px'}} alt="" />
+                  {/* NewsMonkey */}
+                </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                   <span className="navbar-toggler-icon"></span>
                 </button>
